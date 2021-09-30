c2800d4004edb98ee92357271e2674c80d4c7720
SapogMl <st088178@student.spbu.ru>
2021-09-30 09:34:48 +0300

    Create Part_I.cpp

---
+++ 23.09.21/Part_I.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+struct DNode
+{
+	int data;
+	DNode* next;
+	DNode* prev;
+	DNode(int d = 0, DNode* n = nullptr, DNode* p = nullptr) :
+		data(d), next(n), prev(p) {}
+
+};
+
+struct DList
+{
+	DNode* head, * tail;
+	DList() { head = nullptr; tail = nullptr; }
+
+	void add_first(int d)
+	{
+		DNode* t;
+		t = new DNode(d, head, nullptr);
+
+		if (head == nullptr)
+		{
+			head = t;
+			tail = t;
+		}
+		else
+		{
+			head->prev = t;
+			head = t;
+		}
+	}
+
+	void print()
+	{
+		DNode* p = head;
+		while (p != nullptr)
+		{
+			cout << p->data << "\t";
+			p = p->next;
+		}
+		cout << endl;
+	}
+
+	void print_reverse()
+	{
+		DNode* p = tail;
+		while (p != nullptr)
+
+		{
+			cout << p->data << "\t";
+			p = p->prev;
+		}
+		cout << endl;
+	}
+
+	void Print()
+	{
+		print();
+		print_reverse();
+		cout << endl;
+	}
+
+	// ?
+	void add_last(int d)
+	{
+		DNode* tmp;
+		tmp = new DNode(d, nullptr, tail);
+		tail->next = tmp;
+		tail = tmp;
+	}
+
+	// ?
+	void add_after_first(int d)
+	{
+		DNode* tmp;
+		tmp = new DNode(d, head->next, head);
+		head->next->prev = tmp;
+		head->next = tmp;
+	}
+
+	// ?
+	void del_last()
+	{
+		DNode* t = tail;
+		t->prev->next = nullptr;
+		tail = t->prev;
+		delete t;
+	}
+
+	// ?
+	void del_second()
+	{
+		if (head == tail) // если узлов в списке < 2
+		{
+			cout << "ERROR: list too short" << '\n';
+		}
+		else
+		{
+			DNode* t = head->next;
+
+			if (t == tail) // если узлов 2
+			{
+				del_last();
+			}
+			else
+			{
+				head->next = t->next;
+				t->next->prev = t->prev;
+				delete t;
+			}
+		}
+	}
+
+};
+
+int main()
+{
+	DList l;
+
+	l.add_first(3);
+	l.add_first(2);
+	l.add_first(1);
+
+	//l.add_last(4);
+	//l.add_after_first(6);
+	//l.del_last();
+	//l.del_second();
+
+	l.Print();
+
+	return EXIT_SUCCESS;
+}
